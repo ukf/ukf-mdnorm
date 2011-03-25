82189fd52d8c6557fd1c9379de17450fcbfc5da2
Ian Young <ian@iay.org.uk>
2011-03-25 17:33:26 +0000

    Remove incorrect description in header.

--- src/main/java/uk/org/ukfederation/mdnorm/Normalise.java
+++ src/main/java/uk/org/ukfederation/mdnorm/Normalise.java
@@ -1,6 +1,4 @@
 /*
- * Simplified access to the UK federation members.xml database.
- * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
