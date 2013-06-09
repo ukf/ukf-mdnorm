8c95636c7ab8188195e754d9e272f2d3f9d88c0c
Ian Young <ian@iay.org.uk>
2013-06-09 21:36:35 +0100

    Put the voodoo (C) back in to the copyright notice, to match our other packages.

--- src/main/java/uk/org/ukfederation/mdnorm/Normalise.java
+++ src/main/java/uk/org/ukfederation/mdnorm/Normalise.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2013 University of Edinburgh.
+ * Copyright (C) 2013 University of Edinburgh.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
